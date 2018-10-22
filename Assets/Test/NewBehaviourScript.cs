816da151b72aabbaa068b07515a2b379c07a915a
chanyowthea <454982165@qq.com>
2018-10-22 21:07:17 +0800

    fixed camera move bug

---
+++ Assets/Test/NewBehaviourScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Profiling;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+    bool _IsUsed;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Profiler.BeginSample("Judge");
+        for (int i = 0; i < 100; i++)
+        {
+            if (_IsUsed)
+            {
+                _IsUsed = false;
+            }
+        }
+        Profiler.EndSample();
+
+        Profiler.BeginSample("Assign");
+        for (int i = 0; i < 100; i++)
+        {
+            _IsUsed = false;
+        }
+        Profiler.EndSample();
+    }
+}
